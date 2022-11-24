33a7a0fc513eafc884204131a6ed5953d1213fad
bufleek <brunookiprop@gmail.com>
2022-11-24 21:12:59 +0300

    static files configuration

--- AutoRetractableOwning/settings.py
+++ AutoRetractableOwning/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
